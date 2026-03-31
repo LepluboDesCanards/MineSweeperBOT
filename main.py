f875e92d16589ec2c88244372b75247dbe336ba4
Le Plus Beau des Canards <n97583308@gmail.com>
2026-03-31 23:51:18 +0200

    Implement big reveal for empty tiles in MineSweeper
    Added big reveal functionality for empty tiles.

--- main.py
+++ main.py
@@ -32,19 +32,21 @@ class MineSweeper:
                 
                 print(f"[CLICK] -> mouse: ({pyxel.mouse_x}, {pyxel.mouse_y}) ; tile: (x={tile[0]}, y={tile[1]})")
 
-                if self.mine_grid[tile[1]][tile[0]] == 1:
-                    print("GAME OVER !")
-                
-                elif not tile in self.flags:
-                    if tile not in self.reveled_tiles: self.reveled_tiles.append(tile)
-
                 if self.starting_point is None:
                     self.starting_point = tile
                     self.mine_grid = self.plant_mines()
 
+                if self.mine_grid[tile[1]][tile[0]] == 1:
+                    print("GAME OVER !")
+                
+                elif not tile in self.flags:
+                    if tile not in self.reveled_tiles:
+                        self.reveled_tiles.append(tile)
+                        self.big_reveal(tile)
 
+                
                 self.player_grid = self.get_player_grid()
-                print(self.reveled_tiles)
+                #print(self.reveled_tiles)
             
             if pyxel.btnp(pyxel.MOUSE_BUTTON_RIGHT):
                 
@@ -62,7 +64,8 @@ class MineSweeper:
                 x = i*self.tile_scale
                 y = j*self.tile_scale
                 
-                pyxel.rect(x, y, self.tile_scale, self.tile_scale, 1 if self.mine_grid[j][i] else 7)
+                #pyxel.rect(x, y, self.tile_scale, self.tile_scale, 1 if self.mine_grid[j][i] else 7)
+                pyxel.rect(x, y, self.tile_scale, self.tile_scale, 7)
                 pyxel.rectb(x, y, self.tile_scale, self.tile_scale, 6)
 
                 if (i, j) in self.reveled_tiles:
@@ -104,14 +107,31 @@ class MineSweeper:
         return grille
 
 
-    def how_many_mines_around_me(self, tile:tuple[int, int]):
+    def how_many_mines_around_me(self, tile:tuple[int, int], target=1):
 
-        neigbors = [(x, y) for x in range(tile[0]-1, tile[0]+2) for y in range(tile[1]-1, tile[1] + 2) if (0 <= x < len(self.mine_grid)) and (0 <= y < len(self.mine_grid[0])) and ((x, y) != tile) and (self.mine_grid[y][x] == 1)]
-        return len(neigbors), neigbors
+        neighbors = [(x, y) for x in range(tile[0]-1, tile[0]+2) for y in range(tile[1]-1, tile[1] + 2) if (0 <= x < len(self.mine_grid)) and (0 <= y < len(self.mine_grid[0])) and ((x, y) != tile) and (self.mine_grid[y][x] == target)]
+        return len(neighbors), neighbors
     
+
     def get_player_grid(self):
         # -1 -> not reveled, else -> nb of mines around
         return [[self.how_many_mines_around_me((i, j))[0] if (i, j) in self.reveled_tiles else -1 for i in range(self.grid_size[0])] for j in range(self.grid_size[1])]
     
+
+    def big_reveal(self, tile):
+        mines_around = self.how_many_mines_around_me(tile, 1)[0]
+
+        if mines_around == 0:
+            
+            neighbors = self.how_many_mines_around_me(tile, 0)[1]
+            
+            for n in neighbors:
+                if n not in self.reveled_tiles:
+                    self.reveled_tiles.append(n)
+                    
+                    if self.how_many_mines_around_me(n, 1)[0] == 0:
+                        self.big_reveal(n)
+                
+            
 
 M = MineSweeper(256, 256)
